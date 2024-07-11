0eb25411542fc0d0367c5657d5b6cb4735c418f3
Kir Chou <kirchou@google.com>
2024-07-11 17:23:42 +0200

    Initial version that can reach the goal in 8 mins.

--- bot.py
+++ bot.py
@@ -15,30 +15,64 @@ from vendeeglobe import (
 )
 from vendeeglobe.utils import distance_on_surface
 
-
 class Bot:
     """
     This is the ship-controlling bot that will be instantiated for the competition.
     """
 
     def __init__(self):
-        self.team = "TeamName"  # This is your team name
+        self.team = "PyCon Taiwan"  # This is your team name
         # This is the course that the ship has to follow
+        # self.course = []
         self.course = [
+            # France
             Checkpoint(latitude=43.797109, longitude=-11.264905, radius=50),
-            Checkpoint(longitude=-29.908577, latitude=17.999811, radius=50),
-            Checkpoint(latitude=-11.441808, longitude=-29.660252, radius=50),
-            Checkpoint(longitude=-63.240264, latitude=-61.025125, radius=50),
+
+            # France West South
+            # Checkpoint(latitude=17.999811, longitude=-29.908577, radius=50),
+
+            # Brazil East
+            # Checkpoint(latitude=-11.441808, longitude=-29.660252, radius=50),
+
+            # Argentina South
+            # Checkpoint(latitude=-61.025125, longitude=-63.240264, radius=50),
+
+            # Panama
+            Checkpoint(latitude=20.3477869, longitude=-70.4968647, radius=50),
+            Checkpoint(latitude=20.3477869, longitude=-73.4968647, radius=50),
+            Checkpoint(latitude=18, longitude=-75, radius=50),
+            Checkpoint(latitude=9.36, longitude=-80.013, radius=50),
+            Checkpoint(latitude=9.11595, longitude=-79.70804, radius=50),
+            Checkpoint(latitude=8.8, longitude=-79.5, radius=50),
+            Checkpoint(latitude=3, longitude=-79.5, radius=50),
+
+            # Check point 1
             Checkpoint(latitude=2.806318, longitude=-168.943864, radius=1990.0),
-            Checkpoint(latitude=-62.052286, longitude=169.214572, radius=50.0),
+
+            # South of NZ
+            # Checkpoint(latitude=-62.052286, longitude=169.214572, radius=50.0),
+            # North of NZ
+            Checkpoint(latitude=-31.3536, longitude=-195.8203, radius=50.0),
+            Checkpoint(latitude=-48.8068, longitude=-210.5859375, radius=50.0),
+
+            # South of India (very far)
+            # Check point 2
             Checkpoint(latitude=-15.668984, longitude=77.674694, radius=1190.0),
+
+            # South of Africa
             Checkpoint(latitude=-39.438937, longitude=19.836265, radius=50.0),
+
+            # West of Africa
             Checkpoint(latitude=14.881699, longitude=-21.024326, radius=50.0),
+
+            # West of Spain (a bit far)
             Checkpoint(latitude=44.076538, longitude=-18.292936, radius=50.0),
+
+            # Start.
             Checkpoint(
                 latitude=config.start.latitude,
                 longitude=config.start.longitude,
-                radius=5,
+                radius=0,
             ),
         ]
 
@@ -103,10 +137,10 @@ class Bot:
         instructions = Instructions()
 
         # TODO: Remove this, it's only for testing =================
-        current_position_forecast = forecast(
-            latitudes=latitude, longitudes=longitude, times=0
-        )
-        current_position_terrain = world_map(latitudes=latitude, longitudes=longitude)
+        #current_position_forecast = forecast(
+        #    latitudes=latitude, longitudes=longitude, times=0
+        #)
+        #current_position_terrain = world_map(latitudes=latitude, longitudes=longitude)
         # ===========================================================
 
         # Go through all checkpoints and find the next one to reach
